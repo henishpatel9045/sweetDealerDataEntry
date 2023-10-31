96fd99094c10bbf51d1e0b0056e002198a4976b4
Henish Patel <200120107182@git.org.in>
2023-10-31 10:42:47 +0530

    Totals row added in items admin

--- core/admin.py
+++ core/admin.py
@@ -3,8 +3,11 @@ from typing import Any
 from django.contrib import admin
 from django.db.models.query import QuerySet
 from django.http.request import HttpRequest
-from django.template.response import TemplateResponse
-from django.db.models import Sum, F
+from django.contrib.admin.views.main import ChangeList
+from django.db.models import Sum, F, Avg
+from django.utils.html import format_html
+
+from core.constants import ITEM_NAMES
 
 from .models import Order, Item
 
@@ -27,64 +30,112 @@ class OrderModelAdmin(admin.ModelAdmin):
         return obj.dealer.username
 
 
+class TotalAveragesChangeList(ChangeList):
+    # provide the list of fields that we need to calculate averages and totals
+    fields_to_total = [
+        "box_500_gm",
+        "box_1_kg",
+        "quantity_ordered",
+    ]
+
+    def get_total_values(self):
+        """
+        Get the totals
+        """
+        # basically the total parameter is an empty instance of the given model
+        total = Item()
+        total.name = "Totals"  # the label for the totals row
+        orders = Order.objects.all().values_list(*ITEM_NAMES[1])
+        total_500 = sum(
+            i for order in orders for ind, i in enumerate(order) if ind % 2 == 0
+        )
+        total_1000 = sum(
+            i for order in orders for ind, i in enumerate(order) if ind % 2 == 1
+        )
+        quantity_total = total_500 * 0.5 + total_1000
+        total.box_500_gm = str(total_500)
+        total.box_1_kg = str(total_1000)
+        total.quantity_ordered = str(quantity_total)
+
+        return total
+
+    def get_results(self, request):
+        """
+        The model admin gets queryset results from this method
+        and then displays it in the template
+        """
+        super(TotalAveragesChangeList, self).get_results(request)
+        # first get the totals from the current changelist
+        total = self.get_total_values()
+        # small hack. in order to get the objects loaded we need to call for
+        # queryset results once so simple len function does it
+        len(self.result_list)
+        # and finally we add our custom rows to the resulting changelist
+        self.result_list._result_cache.append(total)
+
+
 @admin.register(Item)
 class ItemModelAdmin(admin.ModelAdmin):
     list_display = (
-        "name",
+        "item_name",
         "quantity_available",
         "box_500_gm",
         "box_1_kg",
         "quantity_ordered",
     )
 
+    def get_changelist(self, request, **kwargs):
+        return TotalAveragesChangeList
+
     def changelist_view(self, request):
-        self.ordered_quantity = (
-            Order.objects.all()
-            .annotate(
-                kaju_katri=F("kaju_katri_500") * Decimal("0.5") + F("kaju_katri_1000"),
-                magaj=F("magaj_500") * Decimal("0.5") + F("magaj_1000"),
-                premium_mohanthal=F("premium_mohanthal_500") * Decimal("0.5")
-                + F("premium_mohanthal_1000"),
-                special_toparapak=F("special_toparapak_500") * Decimal("0.5")
-                + F("special_toparapak_1000"),
-                barfi=F("barfi_500") * Decimal("0.5") + F("barfi_1000"),
-                mava_mix_mithai=F("mava_mix_mithai_500") * Decimal("0.5")
-                + F("mava_mix_mithai_1000"),
-                dry_fruite_biscuite=F("dry_fruite_biscuite_500") * Decimal("0.5")
-                + F("dry_fruite_biscuite_1000"),
-                surti_chavanu=F("surti_chavanu_500") * Decimal("0.5")
-                + F("surti_chavanu_1000"),
-                son_papdi=F("son_papdi_500") * Decimal("0.5") + F("son_papdi_1000"),
-            )
-            .aggregate(
-                kaju_katri_sum=Sum("kaju_katri"),
-                magaj_sum=Sum("magaj"),
-                premium_mohanthal_sum=Sum("premium_mohanthal"),
-                special_toparapak_sum=Sum("special_toparapak"),
-                barfi_sum=Sum("barfi"),
-                mava_mix_mithai_sum=Sum("mava_mix_mithai"),
-                dry_fruite_biscuite_sum=Sum("dry_fruite_biscuite"),
-                surti_chavanu_sum=Sum("surti_chavanu"),
-                son_papdi_sum=Sum("son_papdi"),
-            )
-        )
+        self.orders = Order.objects.all().values_list(*ITEM_NAMES[1])
         return super().changelist_view(request)
 
+    def item_name(self, obj):
+        if hasattr(obj, "box_500_gm"):
+            return format_html(
+                '<div class="custom-row" style="font-size: 1.5rem; font-weight: bolder;">Totals</div>'
+            )
+        return obj.name
+
     def quantity_available(self, obj):
+        if hasattr(obj, "box_500_gm"):
+            return format_html(
+                '<div class="custom-row" style="font-size: 1.5rem; font-weight: bold;"></div>'
+            )
         return str(obj.current_quantity) + " KGs"
 
-    def quantity_ordered(self, obj):
-        total = self.ordered_quantity.get(
-            obj.name.replace(" ", "_").lower() + "_sum", 0
-        )
+    def get_item_index(self, item):
+        for index, i in enumerate(ITEM_NAMES[1]):
+            if item.lower().replace(" ", "_") in i:
+                return index
+        return -1
 
+    def quantity_ordered(self, obj):
+        if hasattr(obj, "quantity_ordered"):
+            return format_html(
+                '<div class="custom-row" style="font-size: 1.5rem; font-weight: bolder;">%s KGs</div>'
+                % obj.quantity_ordered
+            )
+        total = float(self.box_500_gm(obj)) * 0.5 + float(self.box_1_kg(obj))
         return str(total) + " KGs"
-    
+
     def box_500_gm(self, obj):
-        qs = Order.objects.all().aggregate(total_500=Sum(f"{obj.name.replace(' ', '_').lower()}_500"))
-        return str(qs.get("total_500", 0)) 
+        if hasattr(obj, "box_500_gm"):
+            return format_html(
+                '<div class="custom-row" style="font-size: 1.5rem; font-weight: bolder;">%s</div>'
+                % obj.box_500_gm
+            )
+        ind = self.get_item_index(obj.name)
+        total = sum(i[ind] for i in self.orders)
+        return str(total)
 
     def box_1_kg(self, obj):
-        qs = Order.objects.all().aggregate(total_1000=Sum(f"{obj.name.replace(' ', '_').lower()}_1000"))
-        return str(qs.get("total_1000", 0))
-    
+        if hasattr(obj, "box_1_kg"):
+            return format_html(
+                '<div class="custom-row" style="font-size: 1.5rem; font-weight: bolder;">%s</div>'
+                % obj.box_1_kg
+            )
+        ind = self.get_item_index(obj.name) + 1
+        total = sum(i[ind] for i in self.orders)
+        return str(total)
